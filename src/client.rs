42518dbc4e698d8cfe1a00c4909b5d18f8b3e3ef
Marcos Taraf <mat20870@uga.edu>
2022-03-03 10:40:34 -0500

    completed send file command from client to server

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{self, Result, Write};
+use std::io::{self, Read, Result, Write};
 use std::net::TcpStream;
 use std::path::Path;
 
@@ -34,10 +34,71 @@ fn send_file(mut stream: &TcpStream, path: &str) -> Result<()> {
     let file = File::open(Path::new(path))?;
     let file_size = file.metadata().unwrap().len();
 
-    let buffer = vec![0; file_size as usize];
-    let written_amt = stream.write(&buffer)?;
-    println!("Bytes written to stream: {}", written_amt);
+    let pathNames: Vec<&str> = path.split("/").collect();
+    let fileName = pathNames[pathNames.len()-1];
+    let mut command = String::from("upload ");
+
+    //command will have upload filename, not file path for server
+    command.push_str(fileName);
+    println!("Command: {}", command);
+
+    //file length string ending with \n so server knows when to stop reading
+    let mut fileLength = file_size.to_string();
+    fileLength.push_str("\n");
+
+    // sending command to server
+    match stream.write(command.as_bytes()){
+        Ok(_) => {
+            println!("Upload Command Sent");
+            ()
+        },
+        Err(e) => {
+            println!("Error sending upload command to server: {}", e);
+        }
+    }
+
+    // sending file size
+    match stream.write(&fileLength.as_bytes()){
+        Ok(_) => {
+            println!("File size sent");
+            ()
+        },
+        Err(e) => {
+            println!("Error sending file size to server: {}", e);
+        }
+    }
+
+    // Reading data from file to send to server
+    let mut buffer = Vec::new();
+    match File::open(&path){
+        Ok(mut file) => {
+            match file.read_to_end(&mut buffer){
+                Ok(_) => (),
+                Err(e) => {
+                    println!("Error reading file to copy data: {}", e);
+                }
+            };
+        },
+        Err(e) => {
+            println!("Error opening file to copy data: {}", e);
+        }
+    };
+
+
+    // sending file data
+    match stream.write(&buffer){
+        Ok(_) => {
+            println!("File data sent");
+            ()
+        },
+        Err(e) => {
+            println!("Error sending file data to server: {}", e);
+        }
+    }
+
+    println!("File sent successfully!");
     Ok(())
+
 }
 
 fn command_loop() {
@@ -48,7 +109,7 @@ fn command_loop() {
                 stream.peer_addr().unwrap()
             );
 
-            let command_list = "\n Commands: \n -- upload \n -- download \n -- help \n -- quit \n";
+            let command_list = "\n Commands: \n -- upload filepath \n -- download \n -- help \n -- quit \n";
 
             println!("{}", command_list);
 
@@ -57,10 +118,11 @@ fn command_loop() {
                 io::stdin()
                     .read_line(&mut user_input)
                     .expect("Error on read");
-                let cmd = user_input.trim();
-                match cmd {
+                let cmd: Vec<&str> = user_input.trim().split_whitespace().collect();
+
+                match cmd[0] {
                     "upload" => {
-                        if let Err(e) = send_file(&stream, "./client_dir/test.txt") {
+                        if let Err(e) = send_file(&stream, cmd[1]) {
                             panic!("The file was not able to be sent: {:?}", e);
                         }
                     }
